5f0fc365d6b0a50acbb43690744aa4f56c65952a
LucasMatos-dev <lucas2003matos4@gmail.com>
2025-02-12 13:07:14 -0300

    2

---
+++ Dia 2/calculadoraDeImpostos2.js
@@ -0,0 +1,46 @@
+let produto = 5600
+let desconto = 0
+let cupom = "pm"
+let estado = "CE" //10, "RJ" 20, "Outros 30"
+let frete = 0
+const icmsMinimo = 12
+const icmsMaximo = 25
+
+if (cupom == "PROMO15") {
+    desconto = 15 
+}
+else if (Math.random () >= 0.5) {
+    desconto = 15
+}
+else {
+    desconto = 0
+}
+
+switch (estado) {
+    case "SP":
+        frete = 10
+        break;
+    case "RJ":
+        frete = 20
+        break
+    default:
+        frete = 30
+        break;
+}
+
+function icms() {
+    return Math.random () * (icmsMaximo - icmsMinimo) + icmsMinimo
+}
+
+let icmsValor = icms()
+let produtoComDesconto = produto - (produto * (desconto/100))
+let produtoComFrete = produtoComDesconto + frete
+let valorFinal = produtoComFrete + (produtoComFrete* (icmsValor/100))
+
+console.log("Preço original do Produto: R$ " + produto)
+console.log ("O Produto teve desconto? "  + (desconto > 0 ? "Sim" : "Não"))
+console.log("Preço do produto com desconto: R$ " + produtoComDesconto.toFixed(2))
+console.log("Preço do frete: R$ " + frete.toFixed(2))
+console.log("Valor do ICMS " + icmsValor.toFixed(2) + "%")
+console.log ("Valor final do produto R$ " + valorFinal.toFixed(2))
+// dp
